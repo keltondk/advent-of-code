9bb00d9382b852593d8128e80ca5af7b8da1c953
keltondk <keck.kelton@gmail.com>
2022-12-01 21:06:18 -0600

    day 1 part 2

--- day_1/src/countCals.java
+++ day_1/src/countCals.java
@@ -1,7 +1,9 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class countCals {
     public static void main(String[] args) throws IOException{
@@ -11,17 +13,34 @@ public class countCals {
         String contents = Files.readString(Paths.get(filePath));
         String[] brokenContents = contents.split("\r\n\r\n");
 
-        int maxCal = 0;
+        List<Integer> maxCals = new ArrayList<>();
+        int minMaxCal = 0;
+
         for (String elf : brokenContents) {
             int currCal = 0;
             String[] foodItems = elf.split("\r\n");
             for (String foodItem : foodItems) {
                 currCal += Integer.parseInt(foodItem);
             }
-            if (currCal > maxCal) {
-                maxCal = currCal;
+            if (maxCals.size() < 3 || currCal > maxCals.get(minMaxCal)) {
+                if (maxCals.size() < 3) {
+                    maxCals.add(currCal);
+                } else {
+                    maxCals.remove(minMaxCal);
+                    maxCals.add(currCal);
+                    minMaxCal = 0;
+                    for (int i = 0; i < maxCals.size(); i++) {
+                        if (maxCals.get(i) < maxCals.get(minMaxCal)) {
+                            minMaxCal = i;
+                        }
+                    }
+                }
             }
         }
-        return maxCal;
+        Integer finalSum = 0;
+        for (Integer cal : maxCals) {
+            finalSum += cal;
+        }
+        return finalSum;
     }
 }
